9d8ae498f1ddbd1eeb5001ba08cce5a74f1e0175
yuri noriki <yurinoriki@hotmail.com>
2024-12-18 14:50:45 -0300

    Usando local_settings.py para sobreescrever variaveis no settings

---
+++ contato/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from contato import models
+
+# Register your models here.
+@admin.register(models.Contato)
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name','phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 10 
+
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
---
+++ contato/urls.py
@@ -0,0 +1,11 @@
+from contato import views
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
