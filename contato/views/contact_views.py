690d54e499e124026bdcb4da882ac31e1c95453b
yuri noriki <yurinoriki@hotmail.com>
2025-01-13 18:11:22 -0300

    Criando a tabela que exibe os contatos no index.html

--- contato/views/contact_views.py
+++ contato/views/contact_views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from contato.models import Contato
 
 # Create your views here.
 
 def index(request):
+    contacts = Contato.objects.all()
+    
+    context = {
+        'contacts':contacts
+    }
+    
     return render(
         request,
-        'contato/index.html'
+        'contato/index.html',
+        context
     )
