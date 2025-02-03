02ad12d5ba5fecd8a2ff3adf792edec64623081e
yuri noriki <yurinoriki@hotmail.com>
2025-02-03 11:06:53 -0300

    Criando mensagem de error com non_field_erros

--- contato/views/contact_forms.py
+++ contato/views/contact_forms.py
@@ -1,9 +1,10 @@
+from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render,get_object_or_404, redirect
 
 from contato.models import Contato
-from django import forms
 
 class ContactForms(forms.ModelForm):
     class Meta:
@@ -11,6 +12,27 @@ class ContactForms(forms.ModelForm):
         fields = (
             'first_name', 'last_name','phone',
         )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            None,
+            ValidationError(
+                "mensagem de error",
+                code = 'invalid'
+            )
+        )
+
+        self.add_error(
+            None,
+            ValidationError(
+                'Mensagem de erro 2',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 def create(request):
     #print(request.method)
